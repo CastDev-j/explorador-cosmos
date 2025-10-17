3347815552edb0875a5ecf4ddbeac4651f1938ba
AndDev <23031429@itcelaya.edu.mx>
2025-10-17 15:51:25 -0600

    refactor: remove title section from NASA Image and Video page for cleaner layout

--- src/modules/nasa-image-video/pages/nasa-image-video-page.tsx
+++ src/modules/nasa-image-video/pages/nasa-image-video-page.tsx
@@ -1,16 +1,9 @@
 import { Container } from "@/components/ui/container";
-import { Title } from "@/components/ui/title";
 import SearchImageAndVideo from "../components/search";
 
 const NasaImageVideoPage = () => {
   return (
     <Container className="space-y-8">
-      <section className="space-y-4">
-        <Title variant="h1" align="center">
-          NASA Image and Video Library
-        </Title>
-      </section>
-
       <section className="space-y-4">
         <SearchImageAndVideo />
       </section>
