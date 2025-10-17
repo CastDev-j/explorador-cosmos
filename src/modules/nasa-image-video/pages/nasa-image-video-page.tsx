61bc5ed906087df03895e59d69d2898d35ffa8ae
AndDev <23031429@itcelaya.edu.mx>
2025-10-17 15:52:01 -0600

    refactor: simplify layout of NasaImageVideoPage by removing unnecessary section wrapper

--- src/modules/nasa-image-video/pages/nasa-image-video-page.tsx
+++ src/modules/nasa-image-video/pages/nasa-image-video-page.tsx
@@ -3,10 +3,8 @@ import SearchImageAndVideo from "../components/search";
 
 const NasaImageVideoPage = () => {
   return (
-    <Container className="space-y-8">
-      <section className="space-y-4">
-        <SearchImageAndVideo />
-      </section>
+    <Container className="space-y-12">
+      <SearchImageAndVideo />
     </Container>
   );
 };
